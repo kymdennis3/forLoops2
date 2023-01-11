365dbe8f366904adc56f34a6e6a4a19a83830a8c
Kimtai <kymdennis3@gmail.com>
2023-01-11 05:45:34 +0300

    Reversed the countdown from 1-10 to 10-1. Added a Happy New year 2023 at the bottom

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //for loop - executes a block of code for a limited amount of time
+
+        for (int i=10; i >=0; i--){
+            System.out.println(i);
+        }
+        System.out.println("Happy New Year 2023!!");
     }
 }
